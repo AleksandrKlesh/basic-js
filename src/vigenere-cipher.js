48767d258324057ba08525775e6346df469a0635
AleksandrKlesh <alex73kl@gmail.com>
2025-01-06 21:49:17 +0700

    feat: solve vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(bool = true) {
+    this.isReversed = bool;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    const keyArr = key.toUpperCase().split('');
+    const messageArr = message.toUpperCase().split('');
+    let keyIndex = 0;
+    let encrypted = [];
+    for (let i = 0; i < messageArr.length; i++) {
+      if (!this.alphabet.includes(messageArr[i])) {
+        encrypted.push(messageArr[i])
+        continue;
+      }
+      if (keyIndex >= keyArr.length) {
+        keyIndex = 0;
+      }
+      const encryptedLetterIndex = this.alphabet.indexOf(messageArr[i]) + this.alphabet.indexOf(keyArr[keyIndex]);
+      const encryptedLetter = this.alphabet[encryptedLetterIndex % 26];
+      encrypted.push(encryptedLetter);
+      keyIndex++;
+    }
+    return this.isReversed ? encrypted.join('') : encrypted.reverse().join('');
+  }
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    const keyArr = key.toUpperCase().split('');
+    const messageArr = message.toUpperCase().split('');
+    let keyIndex = 0;
+    let decrypted = [];
+    for (let i = 0; i < messageArr.length; i++) {
+      if (!this.alphabet.includes(messageArr[i])) {
+        decrypted.push(messageArr[i]);
+        continue;
+      }
+      if (keyIndex >= keyArr.length) {
+        keyIndex = 0;
+      }
+      const decryptedLetterIndex = this.alphabet.indexOf(messageArr[i]) - this.alphabet.indexOf(keyArr[keyIndex]);
+      const decryptedLetter = this.alphabet[(decryptedLetterIndex + 26) % 26]
+      decrypted.push(decryptedLetter);
+      keyIndex++;
+    }
+    return this.isReversed ? decrypted.join('') : decrypted.reverse().join('');
   }
 }
 
